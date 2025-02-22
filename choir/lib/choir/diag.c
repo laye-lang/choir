b4f04cede40ef260bd265992638b99e78f2f5bef
Local Atticus <github@nashiora.com>
2025-02-22 12:15:42 -0500

    Start thinking about diagnostic formatting

--- choir/lib/choir/diag.c
+++ choir/lib/choir/diag.c
@@ -1,13 +1,14 @@
 #include <choir/choir.h>
-#include <stdio.h>
 #include <inttypes.h>
 #include <stdarg.h>
+#include <stdio.h>
+#include <string.h>
 
 CHOIR_API void ch_diag_flush(ch_context* context) {
     if (context->has_issued_diagnostics) {
         fprintf(stderr, "\n");
     }
-    
+
     context->has_issued_diagnostics = true;
 
     // TODO(local): lots of diagnostic formatting to be done soon.
@@ -22,13 +23,23 @@ CHOIR_API void ch_diag_flush(ch_context* context) {
         }
 
         if (diag.location.source != NULL) {
-            fprintf(stderr, "%s:[%"PRIi64":%"PRIi64"]: ", diag.location.source->name, diag.location.offset, diag.location.length);
+            fprintf(stderr, "%s:[%" PRIi64 ":%" PRIi64 "]: ", diag.location.source->name, diag.location.offset, diag.location.length);
         }
 
         fprintf(stderr, "%s\n", diag.message);
     }
 
     context->queued_diagnostics.count = 0;
+}
+
+static ch_string format_diag_message(ch_context* context, ch_diagnostic_kind kind, ch_location location, const char* format, va_list v0) {
+    ch_string result = {0};
+    result.allocator = context->allocator;
+    result.capacity = 1024;
+    result.items = ch_alloc(result.allocator, result.capacity);
+    discard memset(result.items, 0, cast(size_t) result.capacity);
+
+    return result;
 }
 
 CHOIR_API void ch_diag(ch_context* context, ch_diagnostic_kind kind, ch_location location, const char* format, ...) {
